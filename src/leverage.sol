babda9ecd57db8d4143121076d74eee7c82ef2ce
srihar5ha <sri3har5ha@gmail.com>
2024-04-23 00:03:11 +0530

    events added

--- src/leverage.sol
+++ src/leverage.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Leverage {
+
+    event CollateralDeposited(address indexed user, uint256 amount, bool isLong, uint256 leverage);
+    event PositionClosed(address indexed user, uint256 profitLoss, uint256 remainingCollateral);
+    event LeverageUpdated(address indexed user, uint256 newLeverage);
+
+
     IERC20 public collateralToken; // ERC-20 token used as collateral
     address public owner;
 
@@ -14,6 +20,8 @@ contract Leverage {
     }
     uint256 syntheticPriceChange= 15 * 1e18;
     bool isUpward=true; // indicates direction of price change
+    uint256 public minLeverage = 1; // Minimum leverage, typically at least 1
+    uint256 public maxLeverage = 10; // Maximum leverage, can be set to an acceptable upper limit
     
     //one user can have only one position
     mapping(address => Position) public positions; // Track user positions
@@ -39,11 +47,13 @@ contract Leverage {
     function depositCollateral(uint256 amount, bool isLong, uint256 leverage) public {
         require(collateralToken.balanceOf(msg.sender) >= amount, "Insufficient token balance");
         require(amount > 0, "Deposit amount must be greater than 0");
+        require(leverage >= minLeverage && leverage <= maxLeverage, "Leverage out of bounds");
+
         require(positions[msg.sender].collateral == 0, "Open position already exists");
 
         bool success = collateralToken.transferFrom(msg.sender, address(this), amount);
         require(success, "Collateral transfer failed");
-
+        emit CollateralDeposited(msg.sender, amount, isLong, leverage);
         positions[msg.sender] = Position({
             collateral: amount,
             leverage: leverage,
@@ -73,7 +83,7 @@ contract Leverage {
 
     // Clear the position
     delete positions[msg.sender];
-
+    emit PositionClosed(msg.sender, profitLoss, remainingCollateral);
     // Transfer any remaining collateral back to the user
     if (remainingCollateral > 0) {
        collateralToken.transfer(msg.sender, remainingCollateral);
@@ -84,10 +94,14 @@ contract Leverage {
 
     // Update leverage for an open position
     function updateLeverage(uint256 newLeverage) public {
+        require(newLeverage >= minLeverage && newLeverage <= maxLeverage, "Leverage out of bounds");
         Position storage pos = positions[msg.sender];
         require(pos.collateral > 0, "No open position");
 
         pos.leverage = newLeverage;
+
+        emit LeverageUpdated(msg.sender, newLeverage);
+
     }
 
     // Get user's current position details
